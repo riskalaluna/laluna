aa286c36651c9365d61961b480e2d94d19b185ce
riskalaluna <riskalaluna@gmail.com>
2023-03-06 16:02:37 +0700

    first commit

---
+++ LUNA.cpp
@@ -0,0 +1,24 @@
+//library
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){//begin
+
+// numeric p,l,luas
+double p,l,luas;
+//display "masukkan panjang"
+cout<<"masukkan panjang = ";
+//accept p
+cin>>p;
+//display "masukkan lebar"
+cout<<"masukkan lebar = ";
+//accept l
+cin>>l;
+//compute luas=p*l
+luas=p*l
+
+//display "luas persegi panjang adalah", luas
+cout<<"luas persegi panjang adalah "<<luas;
+
+}//end
