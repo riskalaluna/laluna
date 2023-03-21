0f2b307d1e4a736f5832427afe26cc4652dc5175
riskalaluna <riskalaluna@gmail.com>
2023-03-21 20:11:53 +0700

    tipe data double string

---
+++ latihan2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a, double b)
+{
+    return (a = b) /2;
+}
+string status(double d){
+    if (d >=60)
+    return "lulus";
+    else
+    return "tidak lulus";
+}
