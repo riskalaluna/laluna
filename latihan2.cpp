1debf392ba5d45dff744c61b7e60e20c8def2ad4
riskalaluna <riskalaluna@gmail.com>
2023-03-21 20:25:26 +0700

    menambahkan tutup kurawal di Ln5

--- latihan2.cpp
+++ latihan2.cpp
@@ -11,3 +11,25 @@ string status(double d){
     else
     return "tidak lulus";
 }
+string status2(double r, double n){
+    if (r >= 60 && n >= 70)
+    return "lulus";
+    else
+    return "tidak lulus";
+}
+int main()
+{
+    double nilM, nilB;
+
+    cout<< "masukkan nilai matematika : ";
+    cin>> nilM;
+    cout<< "masukkan nilai bahasa inggris : ";
+    cin>> nilB;
+
+    // rerata = (nilM = nilB) / 2;
+    //rata = rerata(nilM , nilB);
+    //st = status (rata);
+    //st = status(rerata(nilM, nilB));
+    cout << "status kelulusannya" << status(rerata(nilM, nilB)) << endl;
+    cout << "status kelulusannya 2" << status2(rerata(nilM, nilB),nilM);
+}
